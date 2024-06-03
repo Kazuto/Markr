29a218d1eff956698d38fc278ca2328629501cde
Kazuto <mail@kazuto.de>
2024-06-03 12:38:11 +0200

    feat(style): initialize and add postcss and prettier

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="min-h-[100svh] bg-gray-900 flex items-center justify-center">
+    <h1 class="text-5xl text-white">Beacon</h1>
   </div>
 </template>
