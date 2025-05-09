46c1679e851197ce1f4797fedfcfe05c36e63f6c
Kai Mayer <mail@kazuto.de>
2025-05-09 13:13:24 +0200

    Update Tailwind to V4 (#3)
    * chore: update dependencies
    * fix: update nuxt config and app.css to Tailwind V4
    * refactor: remove obsolete configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,37 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/app.css"],
+
+  devServer: { port: 4444 },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
-  tailwindcss: {
-    cssPath: "~/assets/app.css",
+
+  modules: ["@nuxt/eslint"],
+
+  ssr: false,
+
+  typescript: {
+    typeCheck: true,
+    strict: true,
+    tsConfig: { include: ["../.eslintrc.*"] },
   },
 
-  options: {
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-      },
+  vite: {
+    build: {
+      minify: true,
     },
+    plugins: [tailwindcss()],
   },
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  sourcemap: {
+    client: true,
+    server: true,
+  },
+
+  compatibilityDate: "2025-05-09",
 });
