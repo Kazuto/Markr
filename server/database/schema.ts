79e6cc931b8a22c6b86ea8c2e9813f039008302f
Kazuto <mail@kazuto.de>
2025-05-12 10:33:57 +0200

    feat: add bookmark group and bookmark schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,12 +1,37 @@
+import { sql } from "drizzle-orm";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const users = sqliteTable("users", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  password: text("password").notNull(),
-  avatar: text("avatar").notNull(),
-  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+const timestamps = {
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .default(sql`(current_timestamp)`),
   updatedAt: integer("updated_at", { mode: "timestamp" }),
   deletedAt: integer("deleted_at", { mode: "timestamp" }),
+};
+
+export const bookmarkGroups = sqliteTable("bookmark_groups", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  uuid: text("uuid")
+    .unique()
+    .notNull()
+    .default(sql`(uuid())`),
+  name: text("name").notNull(),
+  order: integer("order").notNull().default(0),
+  ...timestamps,
+});
+
+export const bookmarks = sqliteTable("bookmarks", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  uuid: text("uuid")
+    .unique()
+    .notNull()
+    .default(sql`(uuid())`),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
+  icon: text("icon"),
+  order: integer("order").notNull().default(0),
+  groupId: integer("group_id")
+    .notNull()
+    .references(() => bookmarkGroups.id),
+  ...timestamps,
 });
