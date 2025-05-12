f92327c408093d4303572a03132ad058e3da03cc
Kazuto <mail@kazuto.de>
2025-05-12 11:34:19 +0200

    feat: add nuxt-auth module

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex min-h-[100svh] items-center justify-center">
-    <h1 class="text-5xl text-white">Beacon</h1>
-  </div>
+  <nuxt-layout>
+    <nuxt-page />
+  </nuxt-layout>
 </template>
+
+<script setup lang="ts"></script>
