cf693370562192eac1ce4b49f2d640cc69556734
Kazuto <mail@kazuto.de>
2025-05-13 16:28:13 +0200

    feat: add card component

---
+++ components/molecules/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="flex w-full max-w-lg flex-col rounded-2xl bg-gray-50 p-8 shadow-xl"
+  >
+    <h2 v-if="title" class="mb-6 text-3xl font-bold text-neutral-800">
+      {{ title }}
+    </h2>
+
+    <div class="text-neutral-300 dark:text-neutral-950">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
