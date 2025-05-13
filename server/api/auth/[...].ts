5e98d5ac68233056113b9045d52cc9501448fb8f
Kazuto <mail@kazuto.de>
2025-05-13 09:22:25 +0200

    feat: create or update user and whitelist record

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -12,19 +12,23 @@ export default NuxtAuthHandler({
     }),
   ],
   callbacks: {
-    async signIn({ user }) {
-      return await handleSignIn(user);
+    async signIn({ user, account }) {
+      return await handleSignIn(user, account);
     },
   },
 });
 
-async function handleSignIn(user: any) {
+async function handleSignIn(user: any, account: any) {
   if (!user.email) return false;
 
   try {
     const whitelist = await findWhitelist(user.email);
 
     if (!whitelist) return false;
+
+    const response = await updateOrCreateUser(user, account);
+
+    await updateWhitelist(whitelist.id, response[0].id);
   } catch (error) {
     console.error(error);
     return false;
@@ -33,8 +37,29 @@ async function handleSignIn(user: any) {
   return true;
 }
 
+async function updateOrCreateUser(user: any, account: any) {
+  return await $fetch("/api/users", {
+    method: "POST",
+    body: {
+      name: user.name,
+      email: user.email,
+      oauth_provider: account.provider,
+      oauth_id: user.id,
+    },
+  });
+}
+
 async function findWhitelist(email: string) {
   const whitelist = await $fetch("/api/whitelist");
 
   return whitelist.find((w) => w.email === email);
+}
+
+async function updateWhitelist(id: number, userId: number) {
+  await $fetch(`/api/whitelist/${id}`, {
+    method: "PUT",
+    body: {
+      userId,
+    },
+  });
 }
