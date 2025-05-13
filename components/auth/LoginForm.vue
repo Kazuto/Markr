ba7ce0f20d7801600ad2a5599dc874d22f6363ff
Kazuto <mail@kazuto.de>
2025-05-13 16:29:04 +0200

    refactor: login page

--- components/auth/LoginForm.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex h-full items-center justify-center">
-    <button @click="signIn('github')">Sign in with GitHub</button>
-  </div>
-</template>
-
-<script setup lang="ts">
-const { signIn } = useAuth();
-</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,35 @@
 <template>
-  <AuthLoginForm />
+  <div
+    class="mx-auto flex h-full w-full max-w-md flex-col justify-center space-y-4"
+  >
+    <molecules-card title="Sign In">
+      <div v-if="providers" class="flex flex-col gap-4">
+        <p>Choose a sign-in method:</p>
+
+        <div class="flex flex-col gap-2">
+          <auth-o-auth-button
+            v-for="provider in providers"
+            :key="provider.name"
+            :provider="provider"
+          >
+            Sign in with {{ provider.displayName }}
+          </auth-o-auth-button>
+        </div>
+
+        <hr class="mb-3 opacity-20" />
+      </div>
+
+      <div class="flex flex-col gap-2">
+        <p>Or sign in with your email and password</p>
+
+        <auth-credential-form />
+      </div>
+    </molecules-card>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { authProviders } = useAuth();
+
+const providers = await authProviders();
+</script>
