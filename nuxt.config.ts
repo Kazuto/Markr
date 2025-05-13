726983a037e515ed6edf0a748657fa2a46cc03f4
Kazuto <mail@kazuto.de>
2025-05-13 12:50:05 +0200

    refactor: remove nuxt-auth, add pocketbase and concurrently

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  auth: {
-    provider: {
-      type: "authjs",
-      trustHost: false,
-      defaultProvider: "github",
-      addDefaultCallbackUrl: true,
-    },
-    sessionRefresh: {
-      enablePeriodically: true,
-      enableOnWindowFocus: true,
-    },
-  },
-
   compatibilityDate: "2025-05-09",
 
   css: ["~/assets/app.css"],
@@ -22,16 +9,16 @@ export default defineNuxtConfig({
   devServer: { port: Number(process.env.PORT) || 3000 },
   devtools: { enabled: true },
 
-  modules: ["@nuxt/eslint", "@sidebase/nuxt-auth"],
+  modules: ["@nuxt/eslint"],
 
   nitro: {
     compressPublicAssets: true,
   },
 
   runtimeConfig: {
-    authSecret: process.env.AUTH_SECRET ?? "",
-    githubClientId: process.env.GITHUB_CLIENT_ID ?? "",
-    githubClientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+    public: {
+      pocketbaseUrl: process.env.POCKETBASE_URL ?? "",
+    },
   },
 
   sourcemap: {
