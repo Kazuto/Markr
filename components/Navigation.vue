ab0065bc05591edc581f8756be4a158cc01b3409
Kazuto <mail@kazuto.de>
2025-05-15 15:45:48 +0200

    refactor: use icons for navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,38 @@
 <template>
-  <div v-if="authenticated" class="absolute right-0 bottom-0 z-50 bg-black/10">
-    <span
-      class="inline-block cursor-pointer p-4 hover:bg-black/20"
-      @click="signOut"
+  <div
+    v-if="authenticated"
+    class="absolute right-0 bottom-0 z-50 overflow-hidden rounded-tl-lg bg-black/10"
+  >
+    <a
+      v-for="(item, index) in menuItems"
+      :key="index"
+      class="inline-flex aspect-square w-16 cursor-pointer items-center justify-center hover:bg-black/20"
+      @click.prevent="item.onClick"
     >
-      Sign out
-    </span>
+      <component :is="item.icon" />
+    </a>
   </div>
 </template>
 
 <script setup>
 const { signOut, authenticated } = useAuth();
+
+const menuItems = ref([
+  {
+    icon: resolveComponent("HomeIcon"),
+    onClick: () => {
+      navigateTo("/");
+    },
+  },
+  {
+    icon: resolveComponent("ShieldIcon"),
+    onClick: () => {
+      navigateTo("/admin");
+    },
+  },
+  {
+    icon: resolveComponent("LogOutIcon"),
+    onClick: signOut,
+  },
+]);
 </script>
