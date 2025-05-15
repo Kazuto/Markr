b4c247ab6174d8dd06fad489b9c24ce14c9745fb
Kazuto <mail@kazuto.de>
2025-05-15 15:36:46 +0200

    feat: add Typography component

---
+++ components/atoms/Typography.vue
@@ -0,0 +1,25 @@
+<template>
+  <component
+    :is
+    :class="{
+      'text-md': size === 'small',
+      'text-xl tracking-tight': size === 'medium',
+      'text-4xl font-medium tracking-tighter': size === 'large',
+    }"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    is?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+    size?: "small" | "medium" | "large";
+  }>(),
+  {
+    is: "h1",
+    size: "medium",
+  },
+);
+</script>
