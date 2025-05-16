45873010bf2a5ac2d4dab3243b398194c14a5abb
Kazuto <mail@kazuto.de>
2025-05-16 12:46:19 +0200

    refactor: remove max-width from card

--- components/molecule/Card.vue
+++ components/molecule/Card.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex w-full max-w-lg flex-col rounded-2xl bg-gray-50 p-8 shadow-xl"
-  >
+  <div class="flex w-full flex-col rounded-2xl bg-gray-50 p-8 shadow-xl">
     <h2 v-if="title" class="mb-6 text-3xl font-bold text-neutral-800">
       {{ title }}
     </h2>
