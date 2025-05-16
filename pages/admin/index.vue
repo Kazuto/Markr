71ef91befa8c463b9abe4e1c961d07b480f27abe
Kazuto <mail@kazuto.de>
2025-05-16 12:45:15 +0200

    feat: add tablist component

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div />
+  <div class="grid grid-cols-[minmax(auto,300px)_1fr] justify-center gap-4">
+    <m-tab-list v-model="activeTab" :tabs="tabs" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Categories from "~/components/admin/CategoryPane.vue";
+import Bookmarks from "~/components/admin/BookmarkPane.vue";
+import Teams from "~/components/admin/TeamPane.vue";
+
+const tabs = {
+  Categories,
+  Bookmarks,
+  Teams,
+};
+
+const activeTab: Ref<keyof typeof tabs> = ref("Categories");
+</script>
