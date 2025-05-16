0271d5f4b597cacdc8141d09bd978c0a4357a2e4
Kazuto <mail@kazuto.de>
2025-05-16 14:38:53 +0200

    fix: lint

--- components/molecule/TabList.vue
+++ components/molecule/TabList.vue
@@ -1,28 +1,29 @@
 <template>
-  <m-card class="px-2 py-2">
-    <ul
+  <ul
+    :class="{
+      'flex space-x-2': horizontal,
+      'space-y-1': !horizontal,
+    }"
+  >
+    <li
+      v-for="(_, tab) in tabs"
+      :key="tab"
+      class="cursor-pointer rounded-lg px-3 py-2 capitalize transition-colors hover:bg-primary-500 hover:text-gray-800"
       :class="{
-        'flex space-x-2': horizontal,
-        'space-y-1': !horizontal,
+        'bg-primary-500 font-bold text-gray-800 hover:bg-primary-500':
+          model === tab,
       }"
+      @click="model = tab"
     >
-      <li
-        v-for="(_, tab) in tabs"
-        :key="tab"
-        class="cursor-pointer rounded-lg px-3 py-2 capitalize transition-colors hover:bg-gray-200"
-        :class="{ 'bg-gray-200 font-bold': model === tab }"
-        @click="model = tab"
-      >
-        {{ tab }}
-      </li>
-    </ul>
-  </m-card>
+      {{ tab }}
+    </li>
+  </ul>
 
   <component :is="tabs[model as keyof Tabs]" />
 </template>
 
 <script setup lang="ts">
-type Tabs = Record<string, unknown>;
+export type Tabs = Record<string, unknown>;
 
 const model = defineModel<keyof Tabs>();
 
