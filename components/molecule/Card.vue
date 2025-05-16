6657830fa87202d00d7d705465374de8e62932da
Kazuto <mail@kazuto.de>
2025-05-16 15:29:58 +0200

    revert: card full height

--- components/molecule/Card.vue
+++ components/molecule/Card.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex h-full w-full flex-col rounded-2xl bg-gray-50 p-8 shadow-xl">
+  <div class="flex w-full flex-col rounded-2xl bg-gray-50 p-8 shadow-xl">
     <h2 v-if="title" class="mb-6 text-3xl font-bold text-neutral-800">
       {{ title }}
     </h2>
 
-    <div class="text-neutral-300 dark:text-neutral-950" v-bind="$attrs">
+    <div class="text-neutral-300 dark:text-neutral-950">
       <slot />
     </div>
   </div>
