e7851fc0492a6eb63239f28e236a71a1f411a7ec
Kazuto <mail@kazuto.de>
2025-06-06 14:05:45 +0200

    fix: default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto h-svh py-4 text-gray-700 dark:text-gray-200">
+  <div class="container mx-auto h-svh py-4 text-gray-700 dark:text-gray-300">
     <slot />
     <Navigation />
   </div>
