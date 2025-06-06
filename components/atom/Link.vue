a41833f9e9834ffb7a51a9afd701e46af557127f
Kazuto <mail@kazuto.de>
2025-06-06 15:13:13 +0200

    fix: group-hover for link

--- components/atom/Link.vue
+++ components/atom/Link.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="cursor-pointer text-primary-500 transition-colors hover:text-amber-400"
+    class="cursor-pointer text-primary-500 transition-colors group-hover:text-amber-400 hover:text-amber-400"
   >
     <slot />
   </a>
